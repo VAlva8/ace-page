dd45d18addf746cd18def4aa863809e851d60dd6
VAlva8 <victor080190@yahoo.com>
2023-10-12 22:57:01 -0400

    Created options for the user to choose from when choosing the fitting

--- src/index.js
+++ src/index.js
@@ -4,11 +4,13 @@ import './index.css';
 import App from './App';
 import Header from './Header';
 import reportWebVitals from './reportWebVitals';
+import FirstSelection from './FirstSelection';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Header/>
+    <FirstSelection/>
   </React.StrictMode>
 );
 
