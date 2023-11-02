8a145dd5a9a1d14ef8ec93734ccdfb12cf958b5d
VAlva8 <victor080190@yahoo.com>
2023-11-02 01:18:15 -0400

    Completed the main function of the website and made it mostly responsive

--- src/Header.js
+++ src/Header.js
@@ -1,4 +1,5 @@
 import './Header.css';
+import FirstSelection from './FirstSelection';
 import React from 'react';
 
 function Header() {
@@ -13,36 +14,57 @@ function Header() {
         document.getElementById("helpPageBlock").style.display="none";
     }
     
-    function hiddenReveal(){
-        window.location.href = "https://youtube.com/";
-    }
-    
     return (
         <div>
             <div className="helpPage" id="helpPageContainer">
-                    <h1 className="helpTitle">Help</h1>
-                    <h2 id="exitButton" onClick={closeHelp}>Exit</h2>
-                    <span className="hiddenButton" onClick={hiddenReveal}></span>
+                    <div className='helpAndExit'>       
+                        <h1 className="helpTitle">Help</h1>
+                        <h2 id="exitButton" onClick={closeHelp}>Exit</h2>
+                    </div>
+                    <h3 className='helpInformation helpDisclaimer'>
+                    Please note, the index leads you to where the fitting SHOULD be. This won't account for the misplacement of the fitting.
+                    </h3>
                 <div className="helpInformationDiv">
-                    <h3 className="helpSubtitle">
+                    <h3 className="helpSubtitle" style={{marginTop: 0,}}>
                         Coordinate System
                     </h3>
                     <p className="helpInformation">
-                        Every result will come with a number, letter, and number indicating exactly where the fitting is.<br/>
-                        <strong>1st Number -</strong> What wall the fitting is on.<br/>
-                        <strong>Letter -</strong> What column the bin for the fitting is on.<br/>
-                        <strong>2nd Number -</strong> What row the bin for the fitting is on.<br/>
+                        Every result will come with a number, letter, and number indicating exactly where the fitting is.
                     </p>
-                    <br/>
+                    <p className='helpInformation indent'>
+                            <strong>1st Number -</strong> What wall the fitting is on.
+                    </p>
+                    <p className='helpInformation indent'>
+                            <strong>Letter -</strong> What column the bin for the fitting is on.
+                    </p>
+                    <p className='helpInformation indent'>
+                            <strong>2nd Number -</strong> What row the bin for the fitting is on.
+                    </p>
                     <a className="helpLink" href="youtube.com">Click here for a visual aid.</a>
                 </div>
                 <div className="helpInformationDiv">
                     <h3 className="helpSubtitle">
-                        Common Fittings
+                        Tips for Searching
                     </h3>
                     <p className="helpInformation">
-
+                        -   When choosing a fitting, anything not FIP or MIP goes first. For example, 
+                        "<strong>Compression</strong> to <strong>FIP</strong>" or "<strong>Flare</strong> to <strong>MIP</strong>".
                     </p>
+                    <p className="helpInformation">
+                        -   When choosing an adapter from FIP to MIP or MIP to FIP, if the fitting is not found then try flipping the order.
+                    </p>
+                    <p className='helpInformation'>
+                        -   If unfamiliar with the acronyms, hover over the first options for the full name.
+                    </p>
+                    <p className='helpInformation'>
+                        -   To reset the page to find a a new fitting, simply refresh the page.
+                    </p>
+                    <p className="helpInformation" style={{marginBottom: "0rem",}}>
+                        -   Needles Valves, Drain Cocks, Saddle Valves, and Ground Plugs were not included in this index.
+                    </p>
+                    <h3 className='helpInformation developedBy'>
+                        Developed by <a className='developedByLink' href='https://www.linkedin.com/in/victor-alvarez-780441274/'>Victor Alvarez</a>.
+                    </h3>
                 </div>
             </div>
             <div id="helpPageBlock">
